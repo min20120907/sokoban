0122810f657d2dc915370de101f497cd0ac0c75c
Cangyu Lin <jefflin.je598@gmail.com>
2020-04-10 11:19:47 +0800

    add failed grid

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QVarLengthArray>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -23,6 +24,27 @@ MainWindow::MainWindow(QWidget *parent) :
     player_facing->setGeometry(50,50,50,50);
     player_facing->setPixmap(*front);
     player_facing->setScaledContents(true);
+    QRectF a ;
+    QPainter* p;
+    drawBackground(p,a);
+}
+void MainWindow::drawBackground(QPainter *painter, const QRectF &rect)
+{
+    const int gridSize = 25;
+
+    qreal left = int(rect.left()) - (int(rect.left()) % gridSize);
+    qreal top = int(rect.top()) - (int(rect.top()) % gridSize);
+
+    QVarLengthArray<QLineF, 100> lines;
+
+    for (qreal x = left; x < rect.right(); x += gridSize)
+        lines.append(QLineF(x, rect.top(), x, rect.bottom()));
+    for (qreal y = top; y < rect.bottom(); y += gridSize)
+        lines.append(QLineF(rect.left(), y, rect.right(), y));
+
+    qDebug() << lines.size();
+
+    painter->drawLines(lines.data(), lines.size());
 }
 void MainWindow::keyPressEvent(QKeyEvent* key){
     if(key->key() == Qt::Key_Up){
